655efeb194712e86d29c2b36035e6d491ceb6f0c
Glenn Gaetos <glenn.williams20@gmail.com>
2025-07-22 21:56:48 +0800

    Adjusted timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 90000,
+  globalTimeout: 600000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
