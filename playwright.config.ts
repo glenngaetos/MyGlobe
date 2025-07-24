e0ae38347217ba412c336c53478adf45c742d2bd
Glenn Gaetos <glenn.williams20@gmail.com>
2025-07-25 00:23:58 +0800

    Enabled chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,10 +36,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    //{
-    //  name: 'chromium',
-    //  use: { ...devices['Desktop Chrome'] },
-    //},
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
 
     //{
     //  name: 'firefox',
@@ -62,10 +62,10 @@ export default defineConfig({
     // },
 
     /* Test against branded browsers. */
-     {
-       name: 'Microsoft Edge',
-       use: { ...devices['Desktop Edge'], channel: 'msedge' },
-     },
+     //{
+     //  name: 'Microsoft Edge',
+     //  use: { ...devices['Desktop Edge'], channel: 'msedge' },
+     //},
      //{
      //  name: 'Google Chrome',
      //  use: { ...devices['Desktop Chrome'], channel: 'chrome' },
